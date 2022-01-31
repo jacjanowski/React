f695a7f4f4683b987d3574bad32c1ec4fb082e08
Jacob Janowski <60745870+jacjanowski@users.noreply.github.com>
2022-01-31 12:44:36 -0600

    finish section 4. done.

--- Project1/my-app/src/components/Expenses/ExpenseItem.js
+++ Project1/my-app/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,9 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import React, {useState} from 'react';
+import React from 'react';
 
 function ExpenseItem(props) {
-
-  const [title, setTitle] = useState(props.title); //is a React Hook
-
-  
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  }
 
   return (
     <Card className="expense-item">
@@ -21,7 +13,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      
     </Card>
   );
 }
