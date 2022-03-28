a20dd787d8c640b06cf9cad533c04e75fa1962dd
Jacob Janowski <60745870+jacjanowski@users.noreply.github.com>
2022-03-28 18:00:41 -0500

    add form for lists

--- Project3/src/components/Users/AddUser.js
+++ Project3/src/components/Users/AddUser.js
@@ -18,7 +18,7 @@ const AddUser = (props) => {
         return;
     }
 
-    console.log(enteredUserName, enteredAge);
+    props.onAddUser(enteredUserName, enteredAge);
     setEnteredUserName('');
     setEnteredAge('');
   };
